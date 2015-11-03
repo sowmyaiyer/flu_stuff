f69017c4e027951e0373fb5da7d795196c8740e9
Sowmya Iyer <sowmyai@bu.edu>
2015-11-03 16:55:41 -0500

    Create MultiRead.java

---
+++ MultiRead.java
@@ -0,0 +1,18 @@
+package multimapper;
+
+public class MultiRead {
+		private int multiplicity;
+		private String[] geneNames;
+		public int getMultiplicity() {
+			return multiplicity;
+		}
+		public void setMultiplicity(int multiplicity) {
+			this.multiplicity = multiplicity;
+		}
+		public String[] getGeneNames() {
+			return geneNames;
+		}
+		public void setGeneNames(String[] geneNames) {
+			this.geneNames = geneNames;
+		}
+}
