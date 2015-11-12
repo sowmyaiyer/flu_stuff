5026b026ea07bf8b5b8085cef17eb346bc5b3cf7
Sowmya Iyer <sowmyai@bu.edu>
2015-11-12 13:48:08 -0500

    Update fluSnatch.R

--- fluSnatch.R
+++ fluSnatch.R
@@ -1,17 +1,3 @@
-assignMultiMappers <- function(row)
-{
-                genes <- as.character(row[3])
-                multiplicity <- as.numeric(row[2])
-                genes_vector <- unlist(strsplit(genes, split=","))
-                sum_snatch_preference <- sum(genes_t_df[genes_vector,"snatch_preference"])
-                sum_snatch_contribution <- sum(genes_t_df[genes_vector,"snatch_contribution_normalized"])
-                for (gene in genes_vector)
-                {
-                        #genes_t_plus_1_df[gene,"snatchReadCounts"] <- genes_t_df[gene,"snatchReadCounts"] + (multiplicity * (genes_t_df[gene,"snatch_preference"]/sum_snatch_preference) *  (genes_t_df[gene,"snatch_contribution_normalized"]/sum_snatch_contribution))
-                        genes_t_plus_1_df[gene,"snatchReadCounts"] <- genes_t_df[gene,"snatchReadCounts"] + (multiplicity * (genes_t_df[gene,"snatch_preference"]/sum_snatch_preference))
-                }
-}
-
 readCounts_file <- commandArgs(TRUE)[1]
 multimapper_file <- commandArgs(TRUE)[2]
 
@@ -19,53 +5,46 @@ df_readCounts <- as.data.frame(read.table(readCounts_file, header=TRUE))
 
 #genes_t_df <- data.frame(row.names=df_readCounts$gene,snatchReadCounts=df_readCounts$snatchReadCounts,capReadCounts=df_readCounts$capReadCounts)
 genes_t_df <- data.frame(row.names=df_readCounts$gene,snatchReadCounts=df_readCounts$snatchReadCounts+1,capReadCounts=NA)
-genes_t_df$snatch_preference <- genes_t_df$snatchReadCounts/sum(genes_t_df$snatchReadCounts)
+#genes_t_df$snatch_preference <- genes_t_df$snatchReadCounts/sum(genes_t_df$snatchReadCounts)
+genes_t_df$snatch_preference <- 1/nrow(genes_t_df)
 genes_t_df$snatch_contribution_unnormalized <- genes_t_df$snatchReadCounts/(genes_t_df$snatchReadCounts+genes_t_df$capReadCounts)
 genes_t_df$snatch_contribution_normalized <- genes_t_df$snatch_contribution_unnormalized/sum(genes_t_df$snatch_contribution_unnormalized)
 genes_t_df$abundance <- (genes_t_df$snatchReadCounts+genes_t_df$capReadCounts)/sum(genes_t_df$snatchReadCounts+genes_t_df$capReadCounts)
+genes_t_df$multireadassignment <- 0.0
 genes_t_plus_1_df <- data.frame(row.names=rownames(genes_t_df),
                                 snatchReadCounts=genes_t_df$snatchReadCounts,
                                 capReadCounts=genes_t_df$capReadCounts,
                                 snatch_preference=genes_t_df$snatch_preference,
                                 snatch_contribution_unnormalized=genes_t_df$snatch_contribution_unnormalized,
                                 snatch_contribution_normalized=genes_t_df$snatch_contribution_normalized,
-                                abundance=genes_t_df$abundance)
-log_l_t <- sum(genes_t_df$snatchReadCounts * (log10(genes_t_df$snatch_preference)))
-cat(log_l_t,"\n")
-log_l_t_plus_1 <- log_l_t + 0.01
+                                abundance=genes_t_df$abundance,
+                                multireadassignment=genes_t_df$multireadassignment)
+diff <- 0.01
 multimap_readCounts <- as.data.frame(read.table(multimapper_file, header=TRUE))
-pdf("/home/si14w/gnearline/flu/scripts/log_l_t_plus_1.pdf")
-v_log_l_t_plus_1 <- numeric()
-while ((log_l_t_plus_1 - log_l_t) > 0.00001)
+print(genes_t_df)
+while (diff > 0.0001)
 {
-#       apply(multimap_readCounts, 1, FUN=assignMultiMappers)
-
+        genes_t_plus_1_df$multireadassignment <- 0
         for (i in 1:nrow(multimap_readCounts))
         {
                 genes <- as.character(multimap_readCounts[i,"genenames"])
                 multiplicity <- multimap_readCounts[i,"multiplicity"]
                 genes_vector <- unlist(strsplit(genes, split=","))
-                cat(i,"\n")
                 sum_snatch_preference <- sum(genes_t_df[genes_vector,"snatch_preference"])
                 sum_snatch_contribution <- sum(genes_t_df[genes_vector,"snatch_contribution_normalized"])
                 for (gene in genes_vector)
                 {
                         #genes_t_plus_1_df[gene,"snatchReadCounts"] <- genes_t_df[gene,"snatchReadCounts"] + (multiplicity * (genes_t_df[gene,"snatch_preference"]/sum_snatch_preference) *  (genes_t_df[gene,"snatch_contribution_normalized"]/sum_snatch_contribution))
-                        genes_t_plus_1_df[gene,"snatchReadCounts"] <- genes_t_df[gene,"snatchReadCounts"]*genes_t_df[gene,"snatch_preference"] + (multiplicity * (genes_t_df[gene,"snatch_preference"]/sum_snatch_preference))
+                        genes_t_plus_1_df[gene,"multireadassignment"] <- genes_t_plus_1_df[gene,"multireadassignment"] + (multiplicity * (genes_t_df[gene,"snatch_preference"]/sum_snatch_preference))
                 }
         }
-
+        genes_t_plus_1_df$snatchReadCounts <- genes_t_df$snatchReadCounts + genes_t_plus_1_df$multireadassignment
         genes_t_plus_1_df$snatch_preference <- genes_t_plus_1_df$snatchReadCounts/sum(genes_t_plus_1_df$snatchReadCounts)
         genes_t_plus_1_df$snatch_contribution_unnormalized <- genes_t_plus_1_df$snatchReadCounts/(genes_t_plus_1_df$snatchReadCounts+genes_t_plus_1_df$capReadCounts)
         genes_t_plus_1_df$snatch_contribution_normalized <- genes_t_plus_1_df$snatch_contribution_unnormalized/sum(genes_t_plus_1_df$snatch_contribution_unnormalized)
         genes_t_plus_1_df$abundance <- (genes_t_plus_1_df$snatchReadCounts+genes_t_plus_1_df$capReadCounts)/sum(genes_t_plus_1_df$snatchReadCounts+genes_t_plus_1_df$capReadCounts)
-        log_l_t <- sum(genes_t_df$snatchReadCounts * (log10(genes_t_df$snatch_preference)))
-        log_l_t_plus_1 <- sum(genes_t_plus_1_df$snatchReadCounts * (log10(genes_t_plus_1_df$snatch_preference)))
-        cat(log_l_t, log_l_t_plus_1,"\n")
+        diff <- sum(abs(genes_t_plus_1_df$snatch_preference - genes_t_df$snatch_preference))
         genes_t_df <- genes_t_plus_1_df
-        v_log_l_t_plus_1 <- c(v_log_l_t_plus_1, log_l_t_plus_1)
 
 }
-cat(v_log_l_t_plus_1,"\n")
-plot(v_log_l_t_plus_1)
-dev.off()
+print(genes_t_df)
